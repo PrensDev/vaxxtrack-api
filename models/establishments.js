a39ce01711ade054ac767ed078e235c92967e92b
Minsc-X1 <72635238+Minsc-X1@users.noreply.github.com>
2021-04-29 20:28:45 +0800

    Update establishments.js

--- models/establishments.js
+++ models/establishments.js
@@ -17,16 +17,82 @@ module.exports = (sequelize, DataTypes) => {
 
   Establishments.init({
     
-    // Model Attributes
-  
-    column: DataTypes.STRING
+    // Model attributes
+
+    establishment_ID: {
+      type          : DataTypes.UUID,
+      allowNull     : false,
+      primaryKey    : true,
+      defaultValue  : Sequelize.UUIDV4,
+      validations   : {
+        notNull: {
+          msg: 'This establishment ID cannot be null'
+        }
+      },
+      comment       : 'This contains the unique identifiers for each record on this table'
+    },
+
+    representative_ID : {
+      type          : DataTypes.UUID,
+      allowNull     : false,
+      validations   : {
+        notNull: {
+          msg: 'This representative ID cannot be null'
+        }
+      },
+      comment       : 'This links the representative on the establishment'
+    }, 
+
+    name: {
+      type          : DataTypes.STRING,
+      allowNull     : false,
+      validate      : {
+        notNull: {
+          msg: 'Name cannot be null',
+        },
+        isAlpha: {
+          msg: 'Must be only letters',
+        }
+      },
+      comment        : 'This contains the name of the representative'
+    },
+
+    type: {
+      type          : DataTypes.STRING,
+      allowNull     : false,
+      validate      : {
+        notNull: {
+          msg: 'type cannot be null',
+        },
+        isIn: {
+          args: [['Company', 'Business', 'Village/Household', 'LGU', 'Organizational']],
+          msg: 'Must be a valid type'
+        }
+      },
+      comment        : 'This contains the type of the establishment'
+    }, 
     
+    address_ID: {
+      type          : DataTypes.UUID,
+      allowNull     : false,
+      validate      : {
+        notNull     : {
+          msg       : 'Address ID must not be null',
+        }
+      },
+      comment       :'This links the address of the establsihment'
+    },
+
   }, {
 
-    // Model Options
+    // Model options
 
     sequelize,
-    modelName: 'Establishments',
+    freezeTableName : true,
+    modelName       : 'Establishments',
+    timestamps      : true,
+    createdAt       : 'created_datetime',
+    updatedAt       : 'updated_datetime',
   });
 
   return Establishments;
