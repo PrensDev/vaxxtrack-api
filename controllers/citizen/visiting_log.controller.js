4437c2b6996f5d81fc5ddcea32c8e003647a827e
danojomari213 <44147684+danojomari213@users.noreply.github.com>
2021-05-28 18:16:41 +0800

    Visiting_logs_controller_fin

--- controllers/citizen/visiting_log.controller.js
+++ controllers/citizen/visiting_log.controller.js
@@ -14,16 +14,39 @@ exports.create = (req, res) => {
             .create({
                 citizen_ID: req.user.user_ID,
                 establishment_ID: req.body.establishment_ID,
-                temperature: 36.1,
+                temperature: req.body.temperature,
                 health_status_log_ID: req.body.health_status_log_ID,
-                purpose: 'Employee'
+                purpose: req.body.purpose, 
             })
             .then((data) => {
-                res.send({
-                    error: false,
-                    data: data,
-                    message: 'A visiting log has been successfuly recorded'
-                });
+                db.Visiting_Logs
+                    .findByPk(data.visiting_log_ID, {
+                        include : [{
+                            model: db.Establishments,
+                            as : "visiting_log_for",
+                            include : [{
+                                model : db.Addresses,
+                                as : "address"
+                            }]
+                        }, {
+                            model: db.Health_Status_Logs,
+                            as : "health_status_log"
+                        }]
+                    })
+                    .then((result) => {
+                        res.send({
+                            err     : false,
+                            data    : result,
+                            msg     : 'A new Visiting Logs has been created!'
+                        })
+                    })
+                    .catch((err) => {
+                        res.status(500).send({
+                            error   : true,
+                            data    : [],
+                            message : ['Opps! Error caught!', `${ err }`],
+                        });
+                    });
             })
             .catch((err) => {
                 res.status(500).send({
@@ -43,9 +66,6 @@ exports.findAll = (req, res, next) => {
         db.Visiting_Logs
             .findAll(
                 {
-                    where: {
-                        citizen_ID: req.user.user_ID
-                    },
                     include: [{
                         model: db.Users,
                         as: 'visiting_log_by',
@@ -90,6 +110,14 @@ exports.findOne = (req, res, next) => {
                     {
                         model: db.Users,
                         as: 'visiting_log_by',
+                        attributes: {
+                            exclude: [
+                                'password',
+                                'added_by',
+                                'created_datetime',
+                                'updated_datetime'
+                            ]
+                        }
                     },
                 ],
             })
