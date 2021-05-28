0fd61293a8bd32cbaf028e71566e263450529079
z-M-A-S-T-z <marct0829@gmail.com>
2021-05-28 19:40:23 +0800

    Update info.controller.js

--- controllers/representative/info.controller.js
+++ controllers/representative/info.controller.js
@@ -4,66 +4,61 @@
  * This controller is for properties related to user information
  */
 
-const db = require("../../models");
-const User = db.User;
-const bcrypt = require("bcrypt");
-
-// return the information of the representative
-exports.getInfo = (req, res, next) => {
-    if(req.user.user_type === 'Representative') {
-        const user_ID = req.user.user_ID;
-        
-        User.findByPk(user_ID)
-            .then((data) => {
-                res.send({
-                  error: false,
-                  data: data,
-                  message: ["Successfully retrieved the data"],
-                });
-            })
-            .catch((err) => {
-                res.status(500).send({
-                  error: true,
-                  data: [],
-                  message: ["Error in retrieving the data"],
-                });
-        });
-    } else {
-        res.sendStatus(403);
-    }
-}
-
-exports.updateInfo = async (req, res) => {
-    const id = req.user.user_ID;
-    req.body.full_name = "";
-
-    if (req.body.password) {
-        req.body.password = await bcrypt.hash(
-            req.body.password,
-            parseInt(process.env.SALT_ROUND)
-        ); 
-    }
-// update the information of the representative
-    User.update_info(req.body, {
-        where : { id : id }
-    }).then((result) => {
-        console.log(result);
-        if (result){
-            // success
-            User.findByPk(id).then((data) => {
-                res.send({
-                    error : false,
-                    data : data,
-                    message : ["Successfully updated"],
-                });
-            });
-        } else {
-            // error in updating
-            res.status(500).send({
-                error: true,
-                data: [],
-                message: ["Error in updating a record"],
-            });
-        }
-    })
-}
+ const db = require("../../models");
+ const User = db.Users;
+ 
+ 
+ // Return the information of the representative
+ exports.getInfo = (req, res, next) => {
+     if(req.user.user_ID === 'Representative') {
+             const id = req.user.user_ID;
+             console.log(id)
+           
+             User.findByPk(id)
+               .then((data) => {
+                 res.send({
+                   error: false,
+                   data: data,
+                   message: ['[Representative] record retrieves successfully'],
+                 });
+             })
+                 .catch((err) => {
+                 res.status(500).send({
+                   error: true,
+                   data: [],
+                   message:
+                     err.errors.map((e) => e.message),
+                 });
+             });
+     } else {
+         res.sendStatus(403);
+     }
+ }
+ 
+ exports.updateInfo = (req, res) => {
+     const id = req.params.id;
+ 
+ // update the information of the Representative
+     User.update_info(req.body, {
+         where : { user_ID : id }
+     }).then((result) => {
+         console.log(result);
+         if (result){
+             // success
+             User.findByPk(id).then((data) => {
+                 res.send({
+                     error : false,
+                     data : data,
+                     message : 'Representative record has been updated.',
+                 });
+             });
+         } else {
+             // error in updating
+             res.status(500).send({
+                 error: true,
+                 data: [],
+                 message: ["Error in updating a record"],
+             });
+         }
+     })
+ }
