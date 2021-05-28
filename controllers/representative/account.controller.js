fb054a2471d57d5aebb41b728352157543cfb786
joshua1515xx <72620425+joshua1515xx@users.noreply.github.com>
2021-05-28 15:57:24 +0800

    Fixed and Tested for Updating Password

--- controllers/representative/account.controller.js
+++ controllers/representative/account.controller.js
@@ -7,49 +7,59 @@
 // Todo: include the properties here
 
 const db = require("../../models");
-const User = db.User;
+const Users = db.Users;
 const bcrypt = require("bcrypt");
 
 
-exports.get_password = (req, res, next) => {
-    if(req.user.user_password === 'password') {
-        res.send({ token_data: req.user });
-    } else {
-        res.sendStatus(403);
-    }
-}
-
-exports.update_password = async (req, res) => {
+exports.update = async (req, res) => 
+{
     const id = req.params.id;
     req.body.full_name = "";
 
-    if (req.body.password) {
-        req.body.password = await bcrypt.hash(
-            req.body.password,
-            parseInt(process.env.SALT_ROUND)
-        ); 
+    if (req.body.password) 
+    {
+        req.body.password = await bcrypt.hash(req.body.password, parseInt(process.env.SALT_ROUNDS));
     }
 
-    User.update_password(req.body, {
-        where : { id : id }
-    }).then((result) => {
-        console.log(result);
-        if (result){
-            // success
-            User.findByPk(id).then((data) => {
-                res.send({
-                    error : false,
-                    data : data,
-                    message : process.env.SUCCESS_UPDATE,
+    Users.update(req.body,
+        {
+            where: {user_ID: id},
+        })
+        .then((result) => 
+        {
+            if(result)
+            {
+                console.log(result);
+                //success
+                Users.findByPk(id).then((data) =>
+                {
+                    res.send(
+                        {
+                            error: false,
+                            data: data,
+                            message: [process.env.SUCCESS_UPDATE],
+                        })
+                })
+            }
+            else
+            {
+                // error in updating
+                res.status(500).send(
+                    {
+                        error: true,
+                        data: [],
+                        message: ["Error in updating a record."],
+                    });
+            }
+        })
+        .catch((err) =>
+        {
+            console.log(err);
+            res.status(500).send(
+                {
+                    error: true,
+                    data: [],
+                    message: err.errors.map((e) => e.message) || process.env.GENERAL_ERROR_MSG,
                 });
-            });
-        } else {
-            // error in updating
-            res.status(500).send({
-                error: true,
-                data: [],
-                message: ["Error in updating your password"],
-            });
-        }
-    })
-}
+        });
+};
