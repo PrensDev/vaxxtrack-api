68edfaa1b934890c24f5f6f85e58e07ac5c0ee8f
Minsc-X1 <72635238+Minsc-X1@users.noreply.github.com>
2021-06-18 19:20:14 +0800

    Update health official info

--- controllers/health_official/info.controller.js
+++ controllers/health_official/info.controller.js
@@ -7,16 +7,17 @@
 
 // Import required packages
 const db     = require('../../models');
+const helper = require('../../helpers/controller.helper');
 
 
-
-// Get Health official Info
+// Return the information of the health official
 exports.getInfo = (req, res, next) => {
-    if(req.user == null || req.user.user_type !== 'Health Official') {
-        res.sendStatus(403);
-    } else {
+
+        // Check if user logged in or logged in but not health official
+        helper.checkAuthorization(req, res, 'Health Official');
+
         db.Users
-            .findByPk(req.user.user_ID,{
+            .findByPk(req.user.user_ID, {
                 attributes: [
                     'first_name',
                     'middle_name',
@@ -24,77 +25,41 @@ exports.getInfo = (req, res, next) => {
                     'suffix_name',
                     'user_type'
                 ]
-            })
-            .then((data) => {
-                if(data) {
-                    res.send({
-                        error: false,
-                        data: data,
-                        message: 'A health official has been identified'
-                    });
-                } else {
-                    res.send({
-                        error: true,
-                        message: 'No health official has been identified'
-                    });
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
+            })   
+            .then((data) => helper.dataResponse(res, data, 'A Record has been identified', 'No Record has been identified'))
+            .catch((err) => helper.errResponse(res, err));
 }
 
-// Update Citizen Info
+// Update health official information
 exports.updateInfo = (req, res, next) => {
 
-    // Check if user logged in or logged in but not Health official
-    if(req.user == null || req.user.user_type !== 'Health Official') {
-        res.sendStatus(403);
-    } else {
+    // Check if user logged in or logged in but not health official
+    helper.checkAuthorization(req, res, 'Health Official');
 
-        // Check first if user ID is existed in database
+        // Check if user ID is existed in database
         db.Users
             .findByPk(req.user.user_ID)
             .then((result) => {
-                if (result) {
 
-                    // Update a health official's record
+                // If no result return empty response
+                if(result == null) helper.emptyDataResponse(res, 'No Record has been identified');
+
+                // Update a health official info
+                db.Users
+                .update(req.body, {
+                    where: {
+                        user_ID: req.user.user_ID
+                    }
+                })
+                .then(() => {
+
+                    // Get health official info
                     db.Users
-                        .update(req.body, {
-                            where: {
-                                user_ID: req.user.user_ID
-                            }
-                        })
-                        .then(() => {
-
-                            // Get health official's record
-                            db.Users
-                                .findByPk(req.user.user_ID)
-                                .then((data) => {
-                                    res.send({
-                                        error: false,
-                                        data: data,
-                                        message: 'Health official record has been successfully updated'
-                                    })
-                                })
-                                .catch((err) => {
-                                    console.log(err);
-                                })
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        })
-                    
-                } else {
-                    res.send({
-                        error: true,
-                        message: 'No health official has been identified'
-                    })
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
+                        .findByPk(req.user.user_ID)
+                        .then((data) => helper.dataResponse(res, data, 'A Record has been successfully updated', 'No changes in the record'))
+                        .catch((err) => helper.errResponse(res, err));
+                })
+                .catch((err) => helper.errResponse(res, err));
+        })
+        .catch((err) => helper.errResponse(res, err));
 }
